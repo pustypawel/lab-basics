ec9d9b11b798ea865c19ef260fbbfbb6b157daf6
ppustelnik <pawel.pustelnik@exorigo-upos.pl>
2021-10-02 19:03:11 +0200

    types, boxing, unboxing

--- src/main/java/pl/wszib/MyClass.java
+++ src/main/java/pl/wszib/MyClass.java
@@ -1,11 +1,20 @@
 package pl.wszib;
 
+import java.math.BigInteger;
+
 public class MyClass extends MyAbstractClass implements MyInterface, MyInterface2 {
     int myInt = 0;
     Integer myInteger = null;
+    BigInteger myBigInteger;
+    Long myLong;
+    long mySmallLong;
 
     @Override
     public void doCalculation() {
+        //nie działa (traktowane jako int, a przekracza jego zakres):
+//        mySmallLong = 100000000000000;
+        myBigInteger = new BigInteger("1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111");
+        mySmallLong = 11000000000000000L;
         doPrint();
         //autoboxing
         Integer myLocalInt = myInt;
