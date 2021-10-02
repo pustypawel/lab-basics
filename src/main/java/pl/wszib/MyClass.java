50a9f8579c0f6ccf0f475feedaf64fbc8be6942e
ppustelnik <pawel.pustelnik@exorigo-upos.pl>
2021-10-02 18:59:17 +0200

    types, boxing, unboxing

--- src/main/java/pl/wszib/MyClass.java
+++ src/main/java/pl/wszib/MyClass.java
@@ -1,9 +1,23 @@
 package pl.wszib;
 
 public class MyClass extends MyAbstractClass implements MyInterface, MyInterface2 {
+    int myInt = 0;
+    Integer myInteger = null;
+
     @Override
     public void doCalculation() {
         doPrint();
+        //autoboxing
+        Integer myLocalInt = myInt;
+        //autounboxing
+        int myLocalInteger = myInteger;
+        Integer myIntegerToChange = 10;
+        doChange(myIntegerToChange);
+        System.out.println("myIntegerToChange = " + myIntegerToChange);
+    }
+
+    public void doChange(Integer myArgInteger) {
+        myArgInteger = 20;
     }
 
     @Override
